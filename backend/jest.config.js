9629580087720b92af7514ca84d57b5710883adb
Unni T A <191848613+unnita1235@users.noreply.github.com>
2026-01-01 08:18:28 +0530

    Remove Jest configuration file

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,14 +1,1 @@
-module.exports = {
-  testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  testMatch: ['**/__tests__/**/*.test.js'],
-  collectCoverageFrom: ['src/**/*.js'],
-  coverageThreshold: {
-    global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
-    },
-  },
-};
+
